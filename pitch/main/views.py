d6e49b40adfa327cda534b2649fc07bfa2394520
yomZsamora <samora.y@adzumi.co.ke>
2019-12-27 12:40:51 +0300

    Route to profile page added in main.views.

--- pitch/main/views.py
+++ pitch/main/views.py
@@ -9,3 +9,9 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+
+@main.route('/profile')
+def profile():
+
+	return render_template('profile.html')
