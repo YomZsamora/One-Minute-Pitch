cd256456078fa9ad45cc98d48da7201ed1f5b85b
yomZsamora <samora.y@adzumi.co.ke>
2019-12-27 11:09:05 +0300

    Done with the registration page.

--- pitch/auth/forms.py
+++ pitch/auth/forms.py
@@ -6,4 +6,5 @@ class RegisterUserForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()], description="Username")
 	email = StringField('Email', validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
-	comfirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+	submit = SubmitField('Create Account')
